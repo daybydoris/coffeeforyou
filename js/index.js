2830cd6bbddaa49c8b111746fd7fe8062a2e5748
GraphicNovel <onegoseen@gmail.com>
2020-11-06 18:10:33 +0900

    [fix] css 중복 제거
    모든 페이지에 중복 적용되어 있는 css 코드를 common.css로 옮겨 중복을 제거함.

--- js/index.js
+++ js/index.js
@@ -1,31 +1,5 @@
 
-    window.addEventListener('DOMContentLoaded',function(){
-        // var menu = document.querySelector('.menu-trigger');
-        // var nav = document.querySelector('nav');
-        // var body = document.querySelector('body');
-        // var menuList = document.querySelectorAll('a');
-        // var btn = document.querySelectorAll('button');
-        
-
-        // //------- 화면 전환 효과 -------
-        // // 시간지연 => body에 active 클래스 추가
-        // setTimeout(function(){
-        //     body.classList.add('active');
-        // },10);
-
-        // // menuList 누를 때 body active 클래스 제거
-        // // menuList href 불러옴
-        // menuList.forEach(function(el){
-        //     el.addEventListener('click', function(e){
-        //         e.preventDefault();
-        //         body.classList.remove('active');
-
-        //         setTimeout(function() {
-        //             location.href = el.href;
-        //         },500);
-        //     });
-        // });
-
+window.addEventListener('DOMContentLoaded',function(){
     //coffee story 게시글 불러오기
     $.ajax({
         url:"data_story.json",
@@ -68,7 +42,6 @@
                 artList += "<h3 class='f_20 title'>";
                 artList += "<a href="+ url +">"+ title +"</a></h3>";
                 artList += "<a href="+ url +"><p class='f_basic contents'>"+ contents +"</p></a></div></article>";
-                //artList += "<a href="+ url +" class='f_basic readMore'>read more</a></article>";
             }
 
             //게시글 최신 날짜순 정렬
